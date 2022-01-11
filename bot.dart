1ece78c376a9a80dc66819cf862786fef761f2d7
arteevraina <arteevraina@gmail.com>
2022-01-11 13:35:15 +0530

    Created separate functions

--- bot.dart
+++ bot.dart
@@ -10,14 +10,8 @@ void main(List<String> args) {
   final List<String> _choices = ["Rock", "Paper", "Scissor"];
 
   while (true) {
-    // Take input from user.
+    // Take input from userChoice.
     int? userChoice = int.parse(stdin.readLineSync()!);
-
-    /// Check if [userChoice] is 0.
-    // If it is 0. Then exit.
-    if (userChoice == 0) {
-      break;
-    }
 
     /// Check if [userChoice] is 5.
     // Print the choice message again.
@@ -27,33 +21,37 @@ void main(List<String> args) {
     }
 
     // +1 Added for min Limit of 1.
-    int computerChoice = Random().nextInt(3) + 1;
-
-    if (userChoice == computerChoice) {
-      // -1 because index starts from zero.
-      stdout.write("You both choose ${_choices[userChoice - 1]}\n\n");
-    } else if (userChoice == 1 &&
-        (computerChoice == 2 || computerChoice == 3)) {
-      // In this case the user will win.
-      stdout.write("You won!!\n");
-      stdout.write(
-          "You choose ${_choices[userChoice - 1]}. Computer choose ${_choices[computerChoice - 1]}\n");
-    } else if (computerChoice == 1 && (userChoice == 2 || userChoice == 3)) {
-      // In this case the user will lose.
-      stdout.write("You lost!!\n");
-      stdout.write(
-          "You choose ${_choices[userChoice - 1]}. Computer choose ${_choices[computerChoice - 1]}\n");
-    } else if (userChoice == 2 && computerChoice == 3) {
-      stdout.write("You lost!!\n");
-      stdout.write(
-          "You choose ${_choices[userChoice - 1]}. Computer choose ${_choices[computerChoice - 1]}\n");
-    } else if (userChoice == 3 && computerChoice == 2) {
-      stdout.write("You won!!\n");
-      stdout.write(
-          "You choose ${_choices[userChoice - 1]}. Computer choose ${_choices[computerChoice - 1]}\n");
-    }
+    giveResult(userChoice, _choices);
 
     stdout.write("Choose Again\n");
+  }
+}
+
+void giveResult(int userChoice, List<String> _choices) {
+  // +1 Added for min Limit of 1.
+  int computerChoice = Random().nextInt(3) + 1;
+
+  if (userChoice == computerChoice) {
+    // -1 because index starts from zero.
+    stdout.write("You both choose ${_choices[userChoice - 1]}\n\n");
+  } else if (userChoice == 1 && (computerChoice == 2 || computerChoice == 3)) {
+    // In this case the user will win.
+    stdout.write("You won!!\n");
+    stdout.write(
+        "You choose ${_choices[userChoice - 1]}. Computer choose ${_choices[computerChoice - 1]}\n");
+  } else if (computerChoice == 1 && (userChoice == 2 || userChoice == 3)) {
+    // In this case the user will lose.
+    stdout.write("You lost!!\n");
+    stdout.write(
+        "You choose ${_choices[userChoice - 1]}. Computer choose ${_choices[computerChoice - 1]}\n");
+  } else if (userChoice == 2 && computerChoice == 3) {
+    stdout.write("You lost!!\n");
+    stdout.write(
+        "You choose ${_choices[userChoice - 1]}. Computer choose ${_choices[computerChoice - 1]}\n");
+  } else if (userChoice == 3 && computerChoice == 2) {
+    stdout.write("You won!!\n");
+    stdout.write(
+        "You choose ${_choices[userChoice - 1]}. Computer choose ${_choices[computerChoice - 1]}\n");
   }
 }
 
